77662f48fe448897da7e04b3ba0840a7a1f8e228
Yatendra <yatendra.gupta_cs.aiml24@gla.ac.in>
2026-07-11 14:50:42 +0530

    Time: 21 ms (31.95%), Space: 47.7 MB (24.51%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+        Map<Character, Integer> need = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int required = need.size();
+        int formed = 0;
+
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            if (need.containsKey(c) &&
+                window.get(c).intValue() == need.get(c).intValue()) {
+                formed++;
+            }
+            while (formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+                char leftChar = s.charAt(left);
+                window.put(leftChar, window.get(leftChar) - 1);
+
+                if (need.containsKey(leftChar) &&
+                    window.get(leftChar) < need.get(leftChar)) {
+                    formed--;
+                }
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
