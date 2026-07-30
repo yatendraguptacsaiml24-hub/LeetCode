037e681d53a3d5b57b8702cf35552cc19adeee95
Yatendra <yatendra.gupta_cs.aiml24@gla.ac.in>
2026-07-30 19:06:47 +0530

    Time: 0 ms (100%), Space: 42.7 MB (97.85%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,9 @@
+class Solution{
+    public int minimumPushes(String word){
+        int ans=0;
+        for(int i=0;i<word.length();i++){
+            ans+=(i/8)+1;
+        }
+        return ans;
+    }
+}
